1f01f9dde29f5235ac94754712b85344f4f45255
jennalei02 <93358942+jennalei02@users.noreply.github.com>
2022-03-29 23:45:52 -0700

    add driver class to test backend

--- Backend/Driver.java
+++ Backend/Driver.java
@@ -1,17 +1,28 @@
+package Project;
 
-public class Driver {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Driver
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		char input1;
 		String inputInfo = new String();
-		int operation2;
 		String line = new String();
+		boolean loggedIn = false;
+	    boolean isAdmin = false;
 
-		//create a linked list to be used in this method.
-		LinkedList list1 = new LinkedList();
-
+		//create objects to be used in this method.
+	    User user = new User();
+	    Cart cart = new Cart();
+	    Menu menu = new Menu();
+	    SpecialDeals sDeals = new SpecialDeals();
+	    
 		try
 		{
 		// print out the menu
@@ -32,50 +43,228 @@ public class Driver {
 		 {
 		  switch (input1)
 		   {
-		    case 'A':   //1. Add String
-		      System.out.print("Please enter a string to add:\n");
-		      String str1 = stdin.readLine().trim();
-		      list1.add(str1);
+		    case 'A':   //1. Log In
+		    	if (loggedIn)
+		    	{
+		    		System.out.print("Already logged in, please log out if a new user.");
+		    	}
+		    	
+		    	else
+		    	{
+		    		System.out.print("Please enter your username:\n");
+				    String username = stdin.readLine().trim();
+				     
+				    System.out.print("Please enter your password:\n");
+				    String password = stdin.readLine().trim();
+				      
+			     	loggedIn = user.login(username, password);
+			     	isAdmin = user.getIsAdmin();
+		    	}
 		      break;
-		    case 'B':   //2. Count the number of occurrences of a string
-		      System.out.print("Please enter a string to count:\n");
-		      inputInfo = stdin.readLine().trim();
-		      operation2=list1.count(inputInfo);
-		      System.out.print("string occurs " + operation2 + " time(s)\n");
+		      
+		    case 'B':   //2. Sign Up
+		    	if (loggedIn)
+		    	{
+		    		System.out.print("Already logged in, please log out if a new user.");
+		    	}
+		    	
+		    	else
+		    	{
+			      System.out.print("Please enter your username:\n");
+			      String username = stdin.readLine().trim();
+			      
+			      System.out.print("Please enter your password:\n");
+			      String password = stdin.readLine().trim();
+			      
+			      System.out.print("Please enter your email:\n");
+			      String email = stdin.readLine().trim();
+			      
+			      System.out.print("If admin, please enter code, otherwise type NULL:\n");
+				  inputInfo = stdin.readLine().trim();
+				  int adminCode = Integer.parseInt(inputInfo);
+			      
+				  // set admin code so admins making accounts can be verified
+			      if (adminCode == 1111) // example code is '1111'
+			      {
+			    	  isAdmin = true;
+			      }
+			      
+			      loggedIn = user.register(isAdmin, username, password, email);
+		    	}
 		      break;
-		    case 'C':   //3. Search for the Index of a String
-		      System.out.print("Please enter a string to search:\n");
-		      inputInfo = stdin.readLine().trim();
-		      operation2=list1.search(inputInfo);
-		      if (operation2 > -1)
-		        System.out.print("string found at " + operation2 + "\n");
-		      else
-		        System.out.print("string not found\n");
+		      
+		    case 'C':   //3. Add Item to Cart
+		    	if (loggedIn)
+		    	{
+		    		// user clicks button to "add item" in the UI
+		    	}
+		    	
+		    	else
+		    	{
+		    		System.out.print("Please log in or sign up first\n");
+		    	}
 		      break;
-		    case 'D':   //4. remove String at an Index
-		      System.out.print("Please enter an index of a string to remove:\n");
-		      inputInfo = stdin.readLine().trim();
-		      int index = Integer.parseInt(inputInfo);
-		      Object removed = list1.remove(index);
-		      if(removed != null) {
-		          System.out.print("The string " + removed + " was removed\n");
-		      }
-		      else {
-		          System.out.println("The index is out of bounds");
-		      }
+		    case 'D':   //4. Remove Item From Cart
+		    	if (loggedIn)
+		    	{
+		    		// user clicks button to "remove item" in the UI
+				    System.out.print("Please enter the id of a string to remove:\n");
+				    inputInfo = stdin.readLine().trim();
+				    int id = Integer.parseInt(inputInfo);
+				    cart.removeItem(id);
+		    	}
+		    	
+		    	else
+		    	{
+		    		System.out.print("Please log in or sign up first\n");
+		    	}
 		      break;
-		    case 'E':  // 5. List Current Size
-		        System.out.print("The current size is " + list1.size() + "\n");
+		      
+		    case 'E':  // 5. Update Quantity
+		    	if (loggedIn)
+		    	{
+		    		// user clicks button to "remove item" in the UI
+				    System.out.print("Please enter the id of a string to remove:\n");
+				    inputInfo = stdin.readLine().trim();
+				    int id = Integer.parseInt(inputInfo);
+				    
+				    System.out.print("Please enter the id of a string to remove:\n");
+				    inputInfo = stdin.readLine().trim();
+				    int quantity = Integer.parseInt(inputInfo);
+				    cart.updateQuantity(id, quantity);
+		    	}
+		    	
+		    	else
+		    	{
+		    		System.out.print("Please log in or sign up first\n");
+		    	}
 		        break;
-		    case 'L':   //6. List Strings
-		      System.out.print(list1.toString());
+		        
+		    case 'F':   // View Menu
+		    	menu.getList();
+			      break;
+			      
+		    case 'G':   //6. Get Item Price
+			    System.out.print("Please enter the id of an item to find the price of:\n");
+			    inputInfo = stdin.readLine().trim();
+			    int id = Integer.parseInt(inputInfo);
+			    
+			    menu.findItem(id);
+			    
 		      break;
-		    case 'F':  //7. Remove last few Strings from End
-		        System.out.print("Please enter a number of elements to remove from the end:\n");
-		        inputInfo = stdin.readLine().trim();
-		        int howManyR = Integer.parseInt(inputInfo);
-		        list1.removeLastFew(howManyR);
+		      
+		    case 'H':  //7. View Special Deals
+		    	sDeals.getMenuItems();
 		        break;
+		       
+		    case 'I':   //Get Cart Total
+		    	if (loggedIn)
+		    	{
+		    		cart.calcTotal();
+		    	}
+		    	else
+		    	{
+		    		System.out.print("Please log in or sign up first\n");
+		    	}
+			      break;
+			      
+		    case 'J':   // Checkout
+		    	if (loggedIn)
+		    	{
+		    		cart.checkOut();
+		    	}
+		    	else
+		    	{
+		    		System.out.print("Please log in or sign up first\n");
+		    	}
+			      break;
+			      
+			      
+			// admin cases
+			      
+		    case 'K':	// Add Item to Menu
+		    	if (isAdmin)
+		    	{
+				      System.out.print("Please enter item ID:\n");
+					  inputInfo = stdin.readLine().trim();
+					  int id = Integer.parseInt(inputInfo);
+				      
+				      System.out.print("Please enter item name:\n");
+				      String name = stdin.readLine().trim();
+				      
+				      System.out.print("Please enter item price:\n");
+				      inputInfo = stdin.readLine().trim();
+					  int price = Integer.parseInt(inputInfo);
+				      
+				      System.out.print("Please enter image for item:\n");
+				      String imageLink = stdin.readLine().trim();
+				      
+				      MenuItem newItem = new MenuItem(id, name, price, 0, imageLink);
+				      
+				      menu.addItem(newItem);
+		    	}
+		    	else
+		    	{
+		    		System.out.print("You do not have access to this action\n");
+		    	}
+			      break;
+			      
+		    case 'L':	//Remove Item from Menu
+		    	if (isAdmin)
+		    	{
+				     System.out.print("Please enter item ID:\n");
+				     inputInfo = stdin.readLine().trim();
+				     int id = Integer.parseInt(inputInfo);
+				     
+				     menu.removeItem(id);
+		    	}
+		    	else
+		    	{
+		    		System.out.print("You do not have access to this action\n");
+		    	}
+			      break;
+			      
+		    case 'M':	// Add Item to Special Deals
+		    	if (isAdmin)
+		    	{
+				      System.out.print("Please enter item ID:\n");
+					  inputInfo = stdin.readLine().trim();
+					  int id = Integer.parseInt(inputInfo);
+				      
+				      System.out.print("Please enter item name:\n");
+				      String name = stdin.readLine().trim();
+				      
+				      System.out.print("Please enter item price:\n");
+				      inputInfo = stdin.readLine().trim();
+					  int price = Integer.parseInt(inputInfo);
+				      
+				      System.out.print("Please enter image for item:\n");
+				      String imageLink = stdin.readLine().trim();
+				      
+				      MenuItem newItem = new MenuItem(id, name, price, 0, imageLink);
+				      
+				      sDeals.addMenuItem(newItem);
+		    	}
+		    	else
+		    	{
+		    		System.out.print("You do not have access to this action\n");
+		    	}
+			      break;
+			      
+		    case 'N':	// Remove Item from Special Deals
+		    	if (isAdmin)
+		    	{
+				     System.out.print("Please enter item ID:\n");
+				     inputInfo = stdin.readLine().trim();
+				     int id = Integer.parseInt(inputInfo);
+				     
+				     sDeals.removeMenuItem(id);
+		    	}
+		    	else
+		    	{
+		    		System.out.print("You do not have access to this action\n");
+		    	}
+			      break;
 
 		    case 'Q':   //Quit
 		      break;
@@ -105,15 +294,23 @@ public class Driver {
 	{
 	System.out.print("Choice\t\tAction\n" +
 	             "------\t\t------\n" +
-	             "A\t\tAdd String\n" +
-	             "B\t\tCount Occurrences\n" +
-	             "C\t\tSearch\n" +
-	             "D\t\tRemove\n" +
-	             "E\t\tSize\n" +
-	             "L\t\tList Strings\n" +
-	             "F\t\tRemove String\n" +
+	             "A\t\tLog In\n" +
+	             "B\t\tCreate an Account\n" +
+	             "C\t\tAdd Item to Cart\n" +
+	             "D\t\tRemove Item From Cart\n" +
+	             "E\t\tUpdate Quantity\n" +
+	             "F\t\tView Menu\n" +
+	             "G\t\tGet Item Price\n" +
+	             "H\t\tView Special Deals\n" +
+	             "I\t\tGet Cart Total\n" +
+	             "J\t\tCheckout\n" +
+	             "K\t\tAdd Item to Menu\n" +
+	             "L\t\tRemove Item from Menu\n" +
+	             "M\t\tAdd Item to Special Deals\n" +
+	             "N\t\tRemove Item from Special Deals\n" +
 	             "Q\t\tQuit\n" +
 	             "?\t\tDisplay Help\n\n");
 	} //end of printMenu()
 
 }
+
