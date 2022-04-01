a92d0f49e7d57a55933070b5da8bc1536c6969ec
Alex-Guha <alexandersguha@gmail.com>
2022-03-31 18:41:07 -0700

    null

--- Alex/application/AddItemsToCart.java
+++ Alex/application/AddItemsToCart.java
@@ -5,10 +5,22 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 public class AddItemsToCart {
 	@FXML
 	private Button button;
+	
+	@FXML
+	private Pane itemName;
+	
+	public void initialize() {
+		 /*Label label = new Label("dango milk");
+		 itemName.getChildren().add(label);*/
+		Label label = new Label(currentOrder.getLastOrderedItem());
+		itemName.getChildren().add(label);
+	}
 	
 	public void navigateToMenu(ActionEvent event) throws IOException {
 		Main m = new Main();
