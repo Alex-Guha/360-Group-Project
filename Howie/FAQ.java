d12dc4075c263eaf164ca45f75eebefb0a928f3b
Hien (Howie) Luong <70129349+howieh2001@users.noreply.github.com>
2022-03-31 18:42:01 -0700

    Add files via upload

---
+++ Howie/FAQ.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+
+public class AddItemsToCart {
+	@FXML
+	private Button button;
+	
+	@FXML
+	private Pane itemName;
+	
+	public void initialize() {
+		 /*Label label = new Label("dango milk");
+		 itemName.getChildren().add(label);*/
+		Label label = new Label(currentOrder.getLastOrderedItem());
+		itemName.getChildren().add(label);
+	}
+	
+	public void navigateToMenu(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Menu.fxml");
+	}
+	
+	public void navigateToOrder(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("OrderNow.fxml");
+	}
+	
+	public void navigateToDeals(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("SpecialDeals.fxml");
+	}
+	
+	public void navigateToAbout(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("AboutUs.fxml");
+	}
+	
+	public void navigateToLogin(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("LogIn.fxml");
+	}
+}
