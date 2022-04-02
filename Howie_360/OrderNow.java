c1a2e8042dd5558ec378c5a34f15956680f528ca
Hien (Howie) Luong <70129349+howieh2001@users.noreply.github.com>
2022-04-01 21:56:33 -0700

    completed

---
+++ Howie_360/OrderNow.java
@@ -0,0 +1,108 @@
+package application;
+
+import javafx.application.Application;
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import java.util.ArrayList;
+
+
+public class OrderNow {
+	
+	@FXML
+	private Button button;
+	
+	@FXML 
+	private Pane orderPane;
+
+	private double totalPrice = 0;
+	
+	public void initialize() {
+		VBox orderDisplay = new VBox();
+		// currentOrder customerOrder[] = customerCart.getCurrentOrder();
+		ArrayList<String> customerOrder = new ArrayList<String>();
+		customerOrder.add("TEST TEST TEST 123");
+		customerOrder.add("TEST TEST TEST 456");
+		customerOrder.add("TEST TEST TEST 789");
+		for(int i = 0; i < customerOrder.size() ; i++) {
+			HBox hBox = new HBox();
+			Label menuItem = new Label(customerOrder.get(i)); // customerOrder[i].getName()
+			hBox.getChildren().add(menuItem);
+			Label quantity = new Label("Quantity: " + 3); // customerOrder[i].getQuantity()
+			hBox.getChildren().add(quantity);
+			Button remove = new Button("Remove");
+			hBox.getChildren().add(remove);
+			orderDisplay.getChildren().add(hBox);
+			
+			//totalPrice = (float) customerOrder[i].getQuantity() * customerOrder[i].getPrice();
+			totalPrice = ((float) 3) * 4.99;
+		}
+		Label total = new Label("Order Total: " + totalPrice);
+		orderDisplay.getChildren().add(total);
+		orderPane.getChildren().add(orderDisplay);
+		
+		/*VBox orderDisplay = new VBox();
+		HBox hBox = new HBox();
+		Label menuItem = new Label("dango milk");
+		hBox.getChildren().add(menuItem);
+		Label quantity = new Label("Quantity: 1");
+		hBox.getChildren().add(quantity);
+		Button remove = new Button("Remove");
+		hBox.getChildren().add(remove);
+		orderDisplay.getChildren().add(hBox);
+			
+		totalPrice = 1.00;
+		Label total = new Label("Order Total: $" + totalPrice);
+		orderDisplay.getChildren().add(total);
+		orderPane.getChildren().add(orderDisplay);*/
+		
+		
+	}
+	
+	
+	public void navigateToPaymentPage() throws IOException {
+		Main m = new Main();
+		m.changeScene("PaymentPage.fxml");
+	}
+	
+	public void navigateToMenu(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Menu.fxml");
+	}
+	
+	public void navigateToOrder(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("OrderNow.fxml");
+	}
+	
+	public void navigateToDeals(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("SpecialDeals.fxml");
+	}
+	
+	public void navigateToFAQ(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("FAQ.fxml");
+	}
+	
+	public void navigateToAbout(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("AboutUs.fxml");
+	}
+	
+	public void navigateToLogin(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Login.fxml");
+	}	
+		
+	
+	
+}
---
+++ Howie_360/Payment.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Payment {
+	@FXML
+	private Button button;
+	
+	public void navigateToMenu(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Menu.fxml");
+	}
+	
+	public void navigateToOrder(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("OrderNow.fxml");
+	}
+	
+	public void navigateToDeals(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("SpecialDeals.fxml");
+	}
+	
+	public void navigateToFAQ(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("FAQ.fxml");
+	}
+	
+	public void navigateToAbout(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("AboutUs.fxml");
+	}
+	
+	public void navigateToLogin(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("Login.fxml");
+	}
+}
